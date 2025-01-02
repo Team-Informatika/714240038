54eaf3bb714d7e329ea5cb9625cb8067bfab3147
Nawal Haromaen <nawalharomaen@gmail.com>
2025-01-02 11:11:59 +0700

    nawal

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
 import {renderHTML} from "https://cdn.jsdelivr.net/gh/jscroot/lib@0.2.0/element.js";
 
 renderHTML('content', 'content/home.html');
+onClick("github",myGithub);
+function myGithub() {
+    window.open('https://github.com/Ameringkseh', 'blank');
+}
+onClick("whatsapp",mywhatsapp);
+function mywhatsapp() {
+    window.open('https://whatsapp.com/6287723806103', 'blank');
+}
+onClick("instagram",myinstagram);
+function myinstagram() {
+    window.open('https://instagram.com/not_moch', 'blank');
+}
